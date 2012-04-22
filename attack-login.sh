84cde9ab6c72704aee2ba16800de37d71acc4e69
Lindsay Kubasik <lindsaykubasik@Lindsays-MacBook-Air-2.local>
2012-04-22 18:25:19 -0400

    modifed logindos

--- attack-login.sh
+++ attack-login.sh
@@ -1,14 +1,14 @@
 #!/usr/bin
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
-node login.js &
+node login.js 1 &
+node login.js 2 &
+node login.js 3 &
+node login.js 4 &
+node login.js 5 &
+node login.js 6 &
+node login.js 7 &
+node login.js 8 &
+node login.js 9 &
+node login.js 10 &
+node login.js 11 &
+node login.js 12 &
 

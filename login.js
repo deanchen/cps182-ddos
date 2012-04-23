2b77a9cdafce30fbc448e740c9be1ec8178a6f9e
Dean Chen <dean.chen@duke.edu>
2012-04-22 20:22:05 -0400

    fixed syntax error

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ var args = process.argv.splice(2);
 var ip = args[0];
 var worker = args[1];
 
-process.on('uncaughtException', function(exception) {console.log(new Date(), "server down"});
+process.on('uncaughtException', function(exception) {console.log(new Date(), "server down")});
 
   var http = require('http');
   var querystring=require('querystring');
